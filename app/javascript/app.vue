588d63d817442692c35fc259b8f1bbddbc6f8918
takeshi furusawa <djpdjp0517@gmail.com>
2020-11-08 17:49:12 +0000

    init

---
+++ app/javascript/app.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <button style='text-align: center;' @click='show = !show'>切り替え</button>
+    <br><br>
+    <transition name='fade' mode='out-in'>
+      <h1 v-if='show' style='text-align: center;'>{{ message }}</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "うんち!!!!!",
+      show: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 1s;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-leave {
+  opacity: 1;
+}
+.fade-leave-active {
+  transition: opacity 1s;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
